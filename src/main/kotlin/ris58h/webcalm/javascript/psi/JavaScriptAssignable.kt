9665945aaaaf5daa166a520c39dbd24e81c6f73a
Ilya Rodionov <ris58h@gmail.com>
2023-05-08 16:46:25 +0300

    JavaScriptAssignable.getUseScope

--- src/main/kotlin/ris58h/webcalm/javascript/psi/JavaScriptAssignable.kt
+++ src/main/kotlin/ris58h/webcalm/javascript/psi/JavaScriptAssignable.kt
@@ -1,5 +1,24 @@
 package ris58h.webcalm.javascript.psi
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.search.LocalSearchScope
+import com.intellij.psi.search.SearchScope
+import com.intellij.psi.util.PsiTreeUtil
 
-class JavaScriptAssignable(node: ASTNode) : JavaScriptIdentifierOwner(node)
+class JavaScriptAssignable(node: ASTNode) : JavaScriptIdentifierOwner(node) {
+    override fun getUseScope(): SearchScope {
+        if (parent is JavaScriptParameter) {
+            val functionBody = PsiTreeUtil.getParentOfType(parent, JavaScriptFunctionDeclaration::class.java)?.body
+            if (functionBody != null) {
+                return LocalSearchScope(functionBody)
+            }
+        }
+        if (parent is JavaScriptVariableDeclaration) {
+            val statementsOwner = PsiTreeUtil.getParentOfType(parent, JavaScriptStatementsOwner::class.java)
+            if (statementsOwner != null) {
+                return LocalSearchScope(statementsOwner)
+            }
+        }
+        return super.getUseScope()
+    }
+}
--- src/main/kotlin/ris58h/webcalm/javascript/psi/JavaScriptFunctionDeclaration.kt
+++ src/main/kotlin/ris58h/webcalm/javascript/psi/JavaScriptFunctionDeclaration.kt
@@ -6,4 +6,7 @@ import com.intellij.psi.util.PsiTreeUtil
 class JavaScriptFunctionDeclaration(node: ASTNode) : JavaScriptIdentifierOwner(node), JavaScriptStatement {
     val parameters: JavaScriptParameters?
         get() = PsiTreeUtil.findChildOfType(this, JavaScriptParameters::class.java)
+
+    val body: JavaScriptFunctionBody?
+        get() = PsiTreeUtil.findChildOfType(this, JavaScriptFunctionBody::class.java)
 }
