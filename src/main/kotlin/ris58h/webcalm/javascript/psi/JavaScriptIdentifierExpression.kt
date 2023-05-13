fcb9f6e4b5b2b4620c6af98fa3686d467eabebcd
Ilya Rodionov <ris58h@gmail.com>
2023-05-13 14:17:01 +0300

    Check if identifier can have reference.

--- src/main/kotlin/ris58h/webcalm/javascript/psi/JavaScriptIdentifierExpression.kt
+++ src/main/kotlin/ris58h/webcalm/javascript/psi/JavaScriptIdentifierExpression.kt
@@ -1,11 +1,20 @@
 package ris58h.webcalm.javascript.psi
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReference
 
 class JavaScriptIdentifierExpression(node: ASTNode) : JavaScriptIdentifierOwner(node) {
     override fun getReference(): PsiReference? {
+        if (!isValidParent(parent)) return null
         val nameIdentifier = this.findChildByClass(JavaScriptIdentifier::class.java) ?: return null
         return JavaScriptIdentifierReference(nameIdentifier.text, this, nameIdentifier.textRangeInParent)
+    }
+
+    private fun isValidParent(parent: PsiElement): Boolean {
+        // TODO: Could we just check if it's not a propertyAssignment?
+        return parent is JavaScriptExpression
+                || parent is JavaScriptParameter
+                || parent is JavaScriptVariableDeclaration
     }
 }
