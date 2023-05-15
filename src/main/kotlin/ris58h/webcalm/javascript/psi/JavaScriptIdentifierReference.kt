84c2591423ad4fb63c87ccfb3c04b65dd0086666
Ilya Rodionov <ris58h@gmail.com>
2023-05-15 13:33:54 +0300

    JavaScriptIdentifierReference.getVariants

--- src/main/kotlin/ris58h/webcalm/javascript/psi/JavaScriptIdentifierReference.kt
+++ src/main/kotlin/ris58h/webcalm/javascript/psi/JavaScriptIdentifierReference.kt
@@ -3,6 +3,7 @@ package ris58h.webcalm.javascript.psi
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.*
 import com.intellij.util.SmartList
+import com.intellij.util.containers.SmartHashSet
 import com.jetbrains.rd.util.forEachReversed
 
 class JavaScriptIdentifierReference(private val name: String, element: PsiElement, rangeInElement: TextRange) :
@@ -14,6 +15,15 @@ class JavaScriptIdentifierReference(private val name: String, element: PsiElemen
             if (it.name == name) declarations.add(it)
         }
         return PsiElementResolveResult.createResults(declarations)
+    }
+
+    override fun getVariants(): Array<Any> {
+        val variants = SmartHashSet<String>()
+        processDeclarations {
+            val name = it.name
+            if (name != null) variants.add(name)
+        }
+        return variants.toTypedArray()
     }
 
     override fun handleElementRename(newElementName: String): PsiElement {
