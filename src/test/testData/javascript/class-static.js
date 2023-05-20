a036791fd23e2dc19e72e9d4a6d7d9c75ec9c94c
Ilya Rodionov <ris58h@gmail.com>
2023-05-20 22:54:57 +0300

    Update class-static.js example.

--- src/test/testData/javascript/class-static.js
+++ src/test/testData/javascript/class-static.js
@@ -1,24 +1,12 @@
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  static displayName = "Point";
-  static distance(a, b) {
-    const dx = a.x - b.x;
-    const dy = a.y - b.y;
-
-    return Math.hypot(dx, dy);
+class ClassWithStaticInitializationBlock {
+  static staticProperty1 = 'Property 1';
+  static staticProperty2;
+  static {
+    this.staticProperty2 = 'Property 2';
   }
 }
 
-const p1 = new Point(5, 5);
-const p2 = new Point(10, 10);
-p1.displayName; // undefined
-p1.distance; // undefined
-p2.displayName; // undefined
-p2.distance; // undefined
-
-console.log(Point.displayName); // "Point"
-console.log(Point.distance(p1, p2)); // 7.0710678118654755
+console.log(ClassWithStaticInitializationBlock.staticProperty1);
+// Expected output: "Property 1"
+console.log(ClassWithStaticInitializationBlock.staticProperty2);
+// Expected output: "Property 2"
