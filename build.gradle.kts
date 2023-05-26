048364c29104d7ade0bb1e22a9bd54d4cf286f9a
Ilya Rodionov <ris58h@gmail.com>
2023-05-26 07:16:32 +0300

    Disable buildSearchableOptions task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,10 @@ tasks {
     }
     getByName("compileKotlin").dependsOn("generateGrammarSource")
 
+    buildSearchableOptions {
+        enabled = false
+    }
+
     signPlugin {
         certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
         privateKey.set(System.getenv("PRIVATE_KEY"))
