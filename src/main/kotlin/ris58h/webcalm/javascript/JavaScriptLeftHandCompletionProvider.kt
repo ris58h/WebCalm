e91ea0bcfd9e242a0949d729099fa3f0ecf83a51
Ilya Rodionov <ris58h@gmail.com>
2023-05-30 21:34:19 +0300

    Remove unused imports.

--- src/main/kotlin/ris58h/webcalm/javascript/JavaScriptLeftHandCompletionProvider.kt
+++ src/main/kotlin/ris58h/webcalm/javascript/JavaScriptLeftHandCompletionProvider.kt
@@ -4,12 +4,7 @@ import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.ProcessingContext
-import ris58h.webcalm.javascript.psi.JavaScriptFile
-import ris58h.webcalm.javascript.psi.JavaScriptIfStatement
-import ris58h.webcalm.javascript.psi.JavaScriptStatement
-import ris58h.webcalm.javascript.psi.JavaScriptStatementsOwner
 
 class JavaScriptLeftHandCompletionProvider : CompletionProvider<CompletionParameters>() {
     override fun addCompletions(
