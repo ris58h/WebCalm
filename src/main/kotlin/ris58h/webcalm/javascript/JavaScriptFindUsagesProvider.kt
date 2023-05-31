1deeccdb0b3f0fe641898f2244c680b6eb1f48cd
Ilya Rodionov <ris58h@gmail.com>
2023-05-31 17:23:43 +0300

    Fix Fund Usages (fixes #2).

--- src/main/kotlin/ris58h/webcalm/javascript/JavaScriptFindUsagesProvider.kt
+++ src/main/kotlin/ris58h/webcalm/javascript/JavaScriptFindUsagesProvider.kt
@@ -4,9 +4,13 @@ import com.intellij.lang.cacheBuilder.WordsScanner
 import com.intellij.lang.findUsages.EmptyFindUsagesProvider
 import com.intellij.psi.PsiElement
 import ris58h.webcalm.javascript.psi.JavaScriptIdentifier
+import ris58h.webcalm.javascript.psi.JavaScriptIdentifierOwner
 
 class JavaScriptFindUsagesProvider : EmptyFindUsagesProvider() {
     override fun getWordsScanner(): WordsScanner = JavaScriptWordsScanner()
 
-    override fun canFindUsagesFor(psiElement: PsiElement): Boolean = psiElement is JavaScriptIdentifier
+    override fun canFindUsagesFor(element: PsiElement): Boolean {
+        return element is JavaScriptIdentifier
+                || element is JavaScriptIdentifierOwner
+    }
 }
---
+++ src/main/kotlin/ris58h/webcalm/javascript/JavaScriptTargetElementEvaluator.kt
@@ -0,0 +1,25 @@
+package ris58h.webcalm.javascript
+
+import com.intellij.codeInsight.TargetElementEvaluatorEx2
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import ris58h.webcalm.javascript.psi.JavaScriptFormalParameter
+import ris58h.webcalm.javascript.psi.JavaScriptIdentifier
+import ris58h.webcalm.javascript.psi.JavaScriptIdentifierExpression
+import ris58h.webcalm.javascript.psi.JavaScriptIdentifierOwner
+
+class JavaScriptTargetElementEvaluator : TargetElementEvaluatorEx2() {
+    override fun getNamedElement(element: PsiElement): PsiElement? {
+        val identifier = PsiTreeUtil.getParentOfType(element, JavaScriptIdentifier::class.java)
+        if (identifier != null) {
+            val parent = identifier.parent
+            if (parent is JavaScriptIdentifierOwner) return parent else return identifier
+        }
+        return null
+    }
+
+    override fun isAcceptableNamedParent(parent: PsiElement): Boolean {
+        // We rely on getNamedElement method.
+        return false
+    }
+}
