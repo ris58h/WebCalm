047a86097c5cbc616422576a3d1c75f304adf344
Ilya Rodionov <ris58h@gmail.com>
2023-06-09 11:54:59 +0300

    CssCalcFunction and CssVarFunction

---
+++ src/main/kotlin/ris58h/webcalm/css/psi/CssCalcFunction.kt
@@ -0,0 +1,6 @@
+package ris58h.webcalm.css.psi
+
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+
+class CssCalcFunction(node: ASTNode) : ASTWrapperPsiElement(node)
