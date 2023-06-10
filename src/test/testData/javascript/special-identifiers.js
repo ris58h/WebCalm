98711d41e443e06591dd0c32c333c3b1100f97d4
Ilya Rodionov <ris58h@gmail.com>
2023-06-10 15:43:19 +0300

    Add special-identifiers.js example.

---
+++ src/test/testData/javascript/special-identifiers.js
@@ -0,0 +1,7 @@
+// TODO: 'as', 'async' and 'from' should be treated as identifiers, not keywords.
+let as; as = 0;
+let async; async = 0;
+let from; from = 0;
+let get; get = 0;
+let of; of = 0;
+let set; set = 0;
