452be5d651f62b06deafdb2d58f33e9175baf82a
Ilya Rodionov <ris58h@gmail.com>
2023-06-20 21:21:31 +0300

    Add class-private.js example.

---
+++ src/test/testData/javascript/class-private.js
@@ -0,0 +1,15 @@
+class ClassWithPrivate {
+  #privateField;
+  #privateFieldWithInitializer = 42;
+
+  #privateMethod() {
+    // …
+  }
+
+  static #privateStaticField;
+  static #privateStaticFieldWithInitializer = 42;
+
+  static #privateStaticMethod() {
+    // …
+  }
+}
