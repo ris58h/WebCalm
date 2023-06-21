778a6d01485bf088440cf2bcecd71fb7dbf27af5
Ilya Rodionov <ris58h@gmail.com>
2023-06-21 12:08:55 +0300

    [JavaScript] Semantic Highlighting.

---
+++ src/main/kotlin/ris58h/webcalm/javascript/JavaScriptRainbowVisitor.kt
@@ -0,0 +1,23 @@
+package ris58h.webcalm.javascript
+
+import com.intellij.codeInsight.daemon.RainbowVisitor
+import com.intellij.codeInsight.daemon.impl.HighlightVisitor
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import ris58h.webcalm.javascript.psi.*
+
+class JavaScriptRainbowVisitor : RainbowVisitor() {
+    override fun suitableForFile(file: PsiFile): Boolean = file is JavaScriptFile
+
+    override fun visit(element: PsiElement) {
+        if (element !is JavaScriptIdentifier) return
+        if (element.introducesName() || element.parent is JavaScriptIdentifierOwner
+            || element.parent is JavaScriptBreakStatement || element.parent is JavaScriptContinueStatement
+            || element.parent is JavaScriptIdentifierExpression) {
+            addInfo(getInfo(element.containingFile, element, element.text, DefaultLanguageHighlighterColors.IDENTIFIER))
+        }
+    }
+
+    override fun clone(): HighlightVisitor = JavaScriptRainbowVisitor()
+}
