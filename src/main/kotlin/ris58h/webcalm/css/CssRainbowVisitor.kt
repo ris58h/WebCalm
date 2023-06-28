2a09ee94b9c428d6bfa4d919a1ef701418100b0d
Ilya Rodionov <ris58h@gmail.com>
2023-06-28 13:28:57 +0300

    [CSS] Semantic highlighting.

---
+++ src/main/kotlin/ris58h/webcalm/css/CssRainbowVisitor.kt
@@ -0,0 +1,21 @@
+package ris58h.webcalm.css
+
+import com.intellij.codeInsight.daemon.RainbowVisitor
+import com.intellij.codeInsight.daemon.impl.HighlightVisitor
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import ris58h.webcalm.css.psi.CssFile
+import ris58h.webcalm.css.psi.CssVariable
+
+class CssRainbowVisitor : RainbowVisitor() {
+    override fun suitableForFile(file: PsiFile): Boolean = file is CssFile
+
+    override fun visit(element: PsiElement) {
+        if (element is CssVariable) {
+            addInfo(getInfo(element.containingFile, element, element.text, DefaultLanguageHighlighterColors.IDENTIFIER))
+        }
+    }
+
+    override fun clone(): HighlightVisitor = CssRainbowVisitor()
+}
