034630271f241826d47b5cb69e126201418bf051
Ilya Rodionov <ris58h@gmail.com>
2023-06-30 19:59:05 +0300

    [CSS] INCOMPLETE_PROPERTY_NAME_PATTERN2

--- src/main/kotlin/ris58h/webcalm/javascript/completion/JavaScriptCompletionContributor.kt
+++ src/main/kotlin/ris58h/webcalm/javascript/completion/JavaScriptCompletionContributor.kt
@@ -2,7 +2,7 @@ package ris58h.webcalm.javascript.completion
 
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionType
-import com.intellij.patterns.PlatformPatterns
+import com.intellij.patterns.PlatformPatterns.*
 import com.intellij.patterns.PsiElementPattern
 import com.intellij.psi.PsiElement
 import ris58h.webcalm.javascript.psi.*
@@ -16,20 +16,24 @@ class JavaScriptCompletionContributor : CompletionContributor() {
 }
 
 private val STATEMENT_IDENTIFIER_PATTERN = identifierExpressionWithParent(
-    PlatformPatterns.psiElement(JavaScriptExpressionSequence::class.java)
-        .withParent(PlatformPatterns.psiElement(JavaScriptExpressionStatement::class.java))
+    psiElement(JavaScriptExpressionSequence::class.java)
+        .withParent(psiElement(JavaScriptExpressionStatement::class.java))
 )
 private val RETURN_IDENTIFIER_PATTERN = identifierExpressionWithParent(
-    PlatformPatterns.psiElement(JavaScriptExpressionSequence::class.java)
-        .withParent(PlatformPatterns.psiElement(JavaScriptReturnStatement::class.java))
+    psiElement(JavaScriptExpressionSequence::class.java)
+        .withParent(psiElement(JavaScriptReturnStatement::class.java))
 )
 private val RIGHT_HAND_IDENTIFIER_PATTERN = identifierExpressionWithParent(
-    PlatformPatterns.psiElement(JavaScriptVariableDeclaration::class.java)
+    psiElement(JavaScriptVariableDeclaration::class.java)
 )
 
 private fun identifierExpressionWithParent(parent: PsiElementPattern.Capture<out PsiElement>): PsiElementPattern.Capture<PsiElement> {
-    return PlatformPatterns.psiElement(JavaScriptTypes.IDENTIFIER)
-        .withParent(PlatformPatterns.psiElement(JavaScriptIdentifier::class.java)
-            .withParent(PlatformPatterns.psiElement(JavaScriptIdentifierExpression::class.java)
-                .withParent(parent)))
+    return psiElement(JavaScriptTypes.IDENTIFIER)
+        .withParent(
+            psiElement(JavaScriptIdentifier::class.java)
+                .withParent(
+                    psiElement(JavaScriptIdentifierExpression::class.java)
+                        .withParent(parent)
+                )
+        )
 }
