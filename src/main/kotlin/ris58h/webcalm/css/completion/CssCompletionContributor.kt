c81163b6774eb4f184b04b5dba855c2c382def25
Ilya Rodionov <ris58h@gmail.com>
2023-06-30 19:59:05 +0300

    [CSS] INCOMPLETE_PROPERTY_NAME_PATTERN2

--- src/main/kotlin/ris58h/webcalm/css/completion/CssCompletionContributor.kt
+++ src/main/kotlin/ris58h/webcalm/css/completion/CssCompletionContributor.kt
@@ -2,7 +2,7 @@ package ris58h.webcalm.css.completion
 
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionType
-import com.intellij.patterns.PlatformPatterns
+import com.intellij.patterns.PlatformPatterns.*
 import com.intellij.psi.PsiErrorElement
 import ris58h.webcalm.css.psi.*
 
@@ -10,28 +10,49 @@ class CssCompletionContributor : CompletionContributor() {
     init {
         extend(
             CompletionType.BASIC,
-            PlatformPatterns.or(PROPERTY_NAME_PATTERN, INCOMPLETE_PROPERTY_NAME_PATTERN),
+            or(PROPERTY_NAME_PATTERN, INCOMPLETE_PROPERTY_NAME_PATTERN, INCOMPLETE_PROPERTY_NAME_PATTERN2),
             CssPropertyNameCompletionProvider()
         )
         extend(
             CompletionType.BASIC,
-            PlatformPatterns.or(TERM_PATTERN, ANY_VALUE_IDENTIFIER_PATTERN),
+            or(TERM_PATTERN, ANY_VALUE_IDENTIFIER_PATTERN),
             CssPropertyValueCompletionProvider()
         )
     }
 }
 
-private val PROPERTY_NAME_PATTERN = PlatformPatterns.psiElement(CssTypes.IDENTIFIER)
-    .withParent(PlatformPatterns.psiElement(CssIdentifier::class.java)
-        .withParent(PlatformPatterns.psiElement(CssProperty::class.java)))
-private val INCOMPLETE_PROPERTY_NAME_PATTERN = PlatformPatterns.psiElement(CssTypes.IDENTIFIER)
-    .withParent(PlatformPatterns.psiElement(PsiErrorElement::class.java)
-        .withParent(PlatformPatterns.psiElement(CssRuleSet::class.java)
-            .withParent(PlatformPatterns.psiElement(CssNestedStatement::class.java))))
+private val PROPERTY_NAME_PATTERN = psiElement(CssTypes.IDENTIFIER)
+    .withParent(
+        psiElement(CssIdentifier::class.java)
+            .withParent(psiElement(CssProperty::class.java))
+    )
+private val INCOMPLETE_PROPERTY_NAME_PATTERN = psiElement(CssTypes.IDENTIFIER)
+    .withParent(
+        psiElement(PsiErrorElement::class.java)
+            .withParent(
+                psiElement(CssRuleSet::class.java)
+                    .withParent(psiElement(CssNestedStatement::class.java))
+            )
+    )
+private val INCOMPLETE_PROPERTY_NAME_PATTERN2 = psiElement(CssTypes.IDENTIFIER)
+    .withParent(
+        psiElement(CssIdentifier::class.java)
+            .withParent(
+                psiElement()
+                    .withParent(
+                        psiElement(CssRuleSet::class.java)
+                            .withParent(psiElement(CssNestedStatement::class.java))
+                    )
+            )
+    )
 
-private val TERM_PATTERN = PlatformPatterns.psiElement(CssTypes.IDENTIFIER)
-    .withParent(PlatformPatterns.psiElement(CssIdentifier::class.java)
-        .withParent(PlatformPatterns.psiElement(CssTerm::class.java)))
-private val ANY_VALUE_IDENTIFIER_PATTERN = PlatformPatterns.psiElement(CssTypes.IDENTIFIER)
-    .withParent(PlatformPatterns.psiElement(CssIdentifier::class.java)
-        .inside(PlatformPatterns.psiElement(CssValue::class.java)))
+private val TERM_PATTERN = psiElement(CssTypes.IDENTIFIER)
+    .withParent(
+        psiElement(CssIdentifier::class.java)
+            .withParent(psiElement(CssTerm::class.java))
+    )
+private val ANY_VALUE_IDENTIFIER_PATTERN = psiElement(CssTypes.IDENTIFIER)
+    .withParent(
+        psiElement(CssIdentifier::class.java)
+            .inside(psiElement(CssValue::class.java))
+    )
