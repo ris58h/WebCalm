cb1052ae34bf5a281e4a6d94c740ab073f9070e1
Ilya Rodionov <ris58h@gmail.com>
2023-06-30 15:33:45 +0300

    CssQuoteHandler

---
+++ src/main/kotlin/ris58h/webcalm/css/CssQuoteHandler.kt
@@ -0,0 +1,37 @@
+package ris58h.webcalm.css
+
+import com.intellij.codeInsight.editorActions.QuoteHandler
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.highlighter.HighlighterIterator
+import ris58h.webcalm.css.psi.CssTypes
+
+class CssQuoteHandler : QuoteHandler {
+    override fun isClosingQuote(iterator: HighlighterIterator, offset: Int): Boolean {
+        if (iterator.tokenType == CssTypes.STRING) {
+            val start = iterator.start
+            val end = iterator.end
+            return end - start >= 1 && offset == end - 1
+        }
+
+        return false
+    }
+
+    override fun isOpeningQuote(iterator: HighlighterIterator, offset: Int): Boolean {
+        // The lexer fails early so single (opening) quote token is considered as an error token.
+        if (iterator.tokenType == CssTypes.UNEXPECTED_CHARACTER) {
+            val char = iterator.document.charsSequence[iterator.start]
+            if (char == '"' || char == '\'') {
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun hasNonClosedLiteral(editor: Editor, iterator: HighlighterIterator, offset: Int): Boolean {
+        return true
+    }
+
+    override fun isInsideLiteral(iterator: HighlighterIterator): Boolean {
+        return false
+    }
+}
