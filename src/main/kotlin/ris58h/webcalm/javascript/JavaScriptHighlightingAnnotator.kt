7fc7ef523d9531319ab783b34ad1e8b09d665fb8
Ilya Rodionov <ris58h@gmail.com>
2024-08-18 22:28:18 +0300

    [JavaScript] const variable highlighting (fixes #22)

--- src/main/kotlin/ris58h/webcalm/javascript/JavaScriptHighlightingAnnotator.kt
+++ src/main/kotlin/ris58h/webcalm/javascript/JavaScriptHighlightingAnnotator.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.project.DumbAware
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
 import ris58h.webcalm.javascript.psi.*
 
 class JavaScriptHighlightingAnnotator : Annotator, DumbAware {
@@ -15,8 +16,33 @@ class JavaScriptHighlightingAnnotator : Annotator, DumbAware {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
         if (element !is JavaScriptIdentifier) return
 
-        val color = when (val parent = element.parent) {
-            is JavaScriptVariableDeclaration -> DefaultLanguageHighlighterColors.LOCAL_VARIABLE
+        var color = colorFromElement(element)
+
+        if (color == null) {
+            val resolved = element.reference?.resolve()
+            if (resolved is JavaScriptIdentifier) {
+                color = colorFromElement(resolved)
+            }
+        }
+
+        if (color != null) {
+            holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                .textAttributes(color)
+                .create()
+        }
+    }
+
+    private fun colorFromElement(element: JavaScriptIdentifier) =
+        when (val parent = element.parent) {
+            is JavaScriptVariableDeclaration -> {
+                val variableStatement = PsiTreeUtil.getParentOfType(parent, JavaScriptVariableStatement::class.java)
+                if (variableStatement != null
+                    && PsiTreeUtil.getParentOfType(variableStatement, JavaScriptStatementsOwner::class.java) is JavaScriptFile) {
+                    DefaultLanguageHighlighterColors.CONSTANT
+                }
+                else DefaultLanguageHighlighterColors.LOCAL_VARIABLE
+            }
+
             is JavaScriptFormalParameter -> DefaultLanguageHighlighterColors.PARAMETER
             is JavaScriptIdentifierExpression -> colorWhenParentIsIdentifierExpression(parent, element)
             is JavaScriptFunctionDeclaration -> {
@@ -39,21 +65,14 @@ class JavaScriptHighlightingAnnotator : Annotator, DumbAware {
             else -> null
         }
 
-        if (color != null) {
-            holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
-                .textAttributes(color)
-                .create()
-        }
-    }
-
     private fun colorWhenParentIsIdentifierExpression(parent: JavaScriptIdentifierExpression, element: JavaScriptIdentifier): TextAttributesKey? {
         return when (val parent2 = parent.parent) {
             is JavaScriptCallExpression -> DefaultLanguageHighlighterColors.FUNCTION_CALL
-            is JavaScriptUpdateExpression -> DefaultLanguageHighlighterColors.REASSIGNED_LOCAL_VARIABLE
-            is JavaScriptAssignmentExpression -> {
-                if (parent2.firstChild === parent) DefaultLanguageHighlighterColors.REASSIGNED_LOCAL_VARIABLE
-                else  null
-            }
+//            is JavaScriptUpdateExpression -> DefaultLanguageHighlighterColors.REASSIGNED_LOCAL_VARIABLE
+//            is JavaScriptAssignmentExpression -> {
+//                if (parent2.firstChild === parent) DefaultLanguageHighlighterColors.REASSIGNED_LOCAL_VARIABLE
+//                else  null
+//            }
             is JavaScriptFormalRestParameter -> DefaultLanguageHighlighterColors.PARAMETER
             else -> {
                 // TODO: check for a param with the same name that can override predefined globals
---
+++ src/test/testData/javascript/reassinged-variables.js
@@ -0,0 +1,8 @@
+var a = 0;
+a = a + 1;
+
+let b = 0;
+b = b + 1;
+
+const c = 0;
+c = c + 1;
