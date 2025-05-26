314254b939c2fe3ea8142421662e9c0ce1dca2ec
Ilya Rodionov <ris58h@gmail.com>
2025-05-26 09:46:49 +0300

    Ensure token types are initialized

--- src/main/kotlin/ris58h/webcalm/css/CssLexer.kt
+++ src/main/kotlin/ris58h/webcalm/css/CssLexer.kt
@@ -2,5 +2,11 @@ package ris58h.webcalm.css
 
 import css3Lexer
 import ris58h.webcalm.antlr.FixedANTLRLexerAdaptor
+import ris58h.webcalm.css.psi.CssTypes
 
-class CssLexer : FixedANTLRLexerAdaptor(CssLanguage, css3Lexer(null))
+class CssLexer : FixedANTLRLexerAdaptor(CssLanguage, css3Lexer(null)) {
+    init {
+        // Ensure token types are initialized
+        CssTypes
+    }
+}
--- src/main/kotlin/ris58h/webcalm/javascript/JavaScriptLexer.kt
+++ src/main/kotlin/ris58h/webcalm/javascript/JavaScriptLexer.kt
@@ -2,5 +2,11 @@ package ris58h.webcalm.javascript
 
 import JavaScriptLexer as JavaScriptANTLRLexer
 import ris58h.webcalm.antlr.FixedANTLRLexerAdaptor
+import ris58h.webcalm.javascript.psi.JavaScriptTypes
 
-class JavaScriptLexer : FixedANTLRLexerAdaptor(JavaScriptLanguage, JavaScriptANTLRLexer(null))
+class JavaScriptLexer : FixedANTLRLexerAdaptor(JavaScriptLanguage, JavaScriptANTLRLexer(null)) {
+    init {
+        // Ensure token types are initialized
+        JavaScriptTypes
+    }
+}
